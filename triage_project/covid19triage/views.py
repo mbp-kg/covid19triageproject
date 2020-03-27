bd66930fa6c62f2552e67fc57024d3bdf65f293c
Troy Farrell <troy.farrell@mbp.kg>
2020-03-27 01:08:45 +0000

    Make it prettier
    Pull in the Django Admin CSS.
    Serve static content from nginx.

--- triage_project/covid19triage/views.py
+++ triage_project/covid19triage/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello")
+    return render(request, "covid19triage/index.html", {})
--- triage_project/triage_project/settings.py
+++ triage_project/triage_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "covid19triage.apps.Covid19TriageConfig",
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = "/opt/triage.mbp.kg/htdocs"
