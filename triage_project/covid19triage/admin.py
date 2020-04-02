e1b6c978b3df94baebb2db1c9bc6a2bd6876ddb0
Troy Farrell <troy.farrell@mbp.kg>
2020-04-02 07:07:17 +0000

    Do not administer Symptoms with Django Admin

--- triage_project/covid19triage/admin.py
+++ triage_project/covid19triage/admin.py
@@ -3,9 +3,7 @@ from django.contrib import admin
 from .models import ContactPerson
 from .models import Patient
 from .models import PatientFactors
-from .models import Symptom
 
 admin.site.register(ContactPerson)
 admin.site.register(Patient)
 admin.site.register(PatientFactors)
-admin.site.register(Symptom)
